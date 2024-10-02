d515f27639c01b20ded0bb3af4801c1701d38abd
ajy3753 <ajy3753@gmail.com>
2024-10-02 18:41:48 +0900

    MemberTBL 업데이트, db 생성

--- java/com/ct/bidsync/member/model/vo/Member.java
+++ java/com/ct/bidsync/member/model/vo/Member.java
@@ -1,5 +1,136 @@
 package com.ct.bidsync.member.model.vo;
 
+import java.sql.Date;
+
 public class Member {
-	// member
+	// 매개변수
+	private int userNo;
+	private String userId;
+	private String userPwd;
+	private String userName;
+	private String gender;
+	private Date birthday;
+	private String email;
+	private String phone;
+	private Date enrollDate;
+	private Date modifyDate;
+	private String status;
+	
+	// 기본 생성자
+	public Member() {
+		super();
+	}
+
+	public Member(int userNo, String userId, String userPwd, String userName, String gender, Date birthday,
+			String email, String phone, Date enrollDate, Date modifyDate, String status) {
+		super();
+		this.userNo = userNo;
+		this.userId = userId;
+		this.userPwd = userPwd;
+		this.userName = userName;
+		this.gender = gender;
+		this.birthday = birthday;
+		this.email = email;
+		this.phone = phone;
+		this.enrollDate = enrollDate;
+		this.modifyDate = modifyDate;
+		this.status = status;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "Member [userNo=" + userNo + ", userId=" + userId + ", userPwd=" + userPwd + ", userName=" + userName
+				+ ", gender=" + gender + ", birthday=" + birthday + ", email=" + email + ", phone=" + phone
+				+ ", enrollDate=" + enrollDate + ", modifyDate=" + modifyDate + ", status=" + status + "]";
+	}
+
+	// setter, getter
+	public int getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserPwd() {
+		return userPwd;
+	}
+
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Date getEnrollDate() {
+		return enrollDate;
+	}
+
+	public void setEnrollDate(Date enrollDate) {
+		this.enrollDate = enrollDate;
+	}
+
+	public Date getModifyDate() {
+		return modifyDate;
+	}
+
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
 }
