69709e31c550f80f128379b11b89bc613fd5fef7
lcw567 <1lcw04181@naver.com>
2024-10-11 11:48:40 +0900

    스토어 방문 버튼 수정

--- webapp/views/resources/js/prdinfoBID.js
+++ webapp/views/resources/js/prdinfoBID.js
@@ -105,7 +105,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const visitStoreButton = document.querySelector('.visit-store');
     if (visitStoreButton) {
         visitStoreButton.addEventListener('click', function() {
-            window.location.href = '../Store/Store.html'; // 스토어 페이지로 이동
+            window.location.href = '../saleList/SalesList.jsp'; // 스토어 페이지로 이동
         });
     }
 
